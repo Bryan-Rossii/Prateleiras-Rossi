11252a8b44f9cb8652e349b6081d01e41690af46
Bryan-Rossii <bryanbonness.5555@gmail.com>
2025-06-12 12:38:36 -0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import PrateleirasExpedicaoRossi from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <PrateleirasExpedicaoRossi />
+  </React.StrictMode>
+);
